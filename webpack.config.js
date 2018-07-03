3d65ed529b7aa6a30a544c71833f5cdb144bc14b
Spencer.Wang <chuanjen.wang@gmail.com>
2018-07-04 00:53:15 +0800

    webpack updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,9 @@
-{
-    const path = require('path');
+const path = require('path');
 
-    module.exports = {
-        entry: './src/index.js',
-        output: {
-            filename: 'bundle.js',
-            path: path.resolve(__dirname,'dist')
-        }
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'dist')
     }
 }
