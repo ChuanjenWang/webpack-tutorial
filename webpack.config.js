3f1511f9e17ea79a1c20795c0f82c58a9f329bb0
Spencer.Wang <chuanjen.wang@gmail.com>
2018-07-03 20:06:09 +0800

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,22 @@
-{
-    const path = require('path');
-
-    module.exports = {
-        entry: './src/index.js',
-        output: {
-            filename: 'bundle.js',
-            path: path.resolve(__dirname,'dist')
-        },
-        module: {
-            rules: [
-                {
-                    use: "babel-loader",
-                    test: /\.js$/,
-                    exclude: /node_modules/
-                },
-                {
-                    use:["style-loader","css-loader"],
-                    test: /\.css$/
-                }
-            ]
-        }
+const path = require('path');
+    
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname,'dist')
+    },
+    module: {
+        rules: [
+            {
+                use: "babel-loader",
+                test: /\.js$/,
+                exclude: /node_modules/
+            },
+            {
+                use:["style-loader","css-loader"],
+                test: /\.css$/
+            }
+        ]
     }
 }
